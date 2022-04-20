87b079af34cfd5abf29c90d51409eac2fafb90b6
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-04-20 15:35:58 +0400

    API v1.2.3

--- florgon-auth-api.js
+++ florgon-auth-api.js
@@ -6,9 +6,9 @@
     Used for working with Florgon auth API.
 
     Current SDK version:
-        v1.1.3
+        v1.2.3
     Expected auth API version: 
-        v1.1.3
+        v1.2.3
 
     Source code:
         https://github.com/florgon/auth-sdk
@@ -22,7 +22,7 @@
 */
 
 // Settings.
-const AUTH_API_EXPECTED_VERSION = "1.1.3";
+const AUTH_API_EXPECTED_VERSION = "1.2.3";
 const AUTH_API_ENDPOINT_URL = "https://api.florgon.space/auth/v1/";
 const AUTH_API_HTTP_METHOD = "GET";
 const AUTH_API_DEFAULT_HEADERS = {
@@ -42,23 +42,67 @@ const AUTH_API_DEFAULT_HEADERS = {
 const authApiErrorCode = {
     AUTH_USERNAME_TAKEN: 0,
     AUTH_EMAIL_TAKEN: 1,
+
     AUTH_INVALID_TOKEN: 10,
     AUTH_EXPIRED_TOKEN:  11,
+
     AUTH_INVALID_CREDENTIALS: 20,
     AUTH_REQUIRED: 21,
     AUTH_EMAIL_INVALID: 30,
     AUTH_PASSWORD_INVALID: 31,
     AUTH_USERNAME_INVALID: 32,
+
     API_INVALID_REQUEST: 40,
+    API_NOT_IMPLEMENTED: 41,
+
+    CFT_INVALID_TOKEN: 50,
+    CFT_EMAIL_NOT_FOUND:  51,
+    CFT_EMAIL_ALREADY_CONFIRMED: 52,
+    
+    OAUTH_CLIENT_NOT_FOUND: 60,
+    OAUTH_CLIENT_FORBIDDEN: 61,
 }
 
+
 // Methods wrapper.
+// Other.
+const authMethodChangelog = (onSuccess=undefined, onError=undefined) => authApiRequest("changelog", "", "", onSuccess, onError);
+// User / token.
 const authMethodVerify = (accessToken, onSuccess=undefined, onError=undefined) => authApiRequest("verify", "", accessToken, onSuccess, onError);
 const authMethodUser = (accessToken, onSuccess=undefined, onError=undefined) => authApiRequest("user", "", accessToken, onSuccess, onError);
+// Sign-in/up
 const authMethodSignin = (login, password, onSuccess=undefined, onError=undefined) => authApiRequest("signin", "login=" + login + "&password=" + password, "", onSuccess, onError);
-const authMethodSignup = (username, email, password, onSuccess=undefined, onError=undefined) => authApiRequest("signup", "username=" + username + "&email=" + email + "&password=" + password, "", onSuccess, onError);
+const authMethodSignup = (username, email, password, onSuccess=undefined, onError=undefined) => {
+    authApiRequest("signup", "username=" + username + "&email=" + email + "&password=" + password, "", onSuccess, onError);
+}
+// Email.
 const authMethodEmailConfirm = (confirmationToken, onSuccess=undefined, onError=undefined) => authApiRequest("email/confirm", "cft=" + confirmationToken, "", onSuccess, onError);
 const authMethodEmailResendConfirmation = (accessToken, onSuccess=undefined, onError=undefined) => authApiRequest("email/resend_confirmation", "", accessToken, onSuccess, onError);
+// OAuth.
+const authMethodOAuthAuthorize = (clientId, redirectUri, responseType, scope, state, onSuccess=undefined, onError=undefined) => {
+    authApiRequest("oauth/authorize", `client_id=${clientId}&state=${state}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`, "", onSuccess, onError);
+}
+const authMethodOAuthToken = (code, onSuccess=undefined, onError=undefined) => authApiRequest("oauth/token", "code=" + code, "", onSuccess, onError);
+const authMethodOAuthDirect = (clientId, clientSecret, username, email, password, onSuccess=undefined, onError=undefined) => {
+    authApiRequest("oauth/direct", `client_id=${clientId}&client_secret=${clientSecret}&username=${username}&email=${email}&password=${password}`, "", onSuccess, onError);
+}
+// OAuth client.
+const authMethodOAuthClientGet = (clientId, onSuccess=undefined, onError=undefined) => authApiRequest("oauth/client/get", "client_id=" + clientId, "", onSuccess, onError);
+const authMethodOAuthClientNew = (displayName, accessToken, onSuccess=undefined, onError=undefined) => {
+    authApiRequest("oauth/client/new", "display_name=" + displayName, accessToken, onSuccess, onError);
+}
+const authMethodOAuthClientExpire = (clientId, accessToken, onSuccess=undefined, onError=undefined) => {
+    authApiRequest("oauth/client/expire", "client_id=" + clientId, accessToken, onSuccess, onError);
+}
+
+// Getter for OAuth authorization url.
+// Use this for own redirect, or method below to direct redirect.
+function authApiGetOAuthAuthorizationUrl(clientId, redirectUri, responseType="token", scope="", state=""){
+    return _buildRequestURL("oauth/authorize", `client_id=${clientId}&state=${state}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`)
+}
+function authApiRedirectOAuthAuthorization(clientId, redirectUri, responseType="token", scope="", state=""){
+    window.location.href = authApiGetOAuthAuthorizationUrl(clientId, redirectUri, responseType, scope, state);
+}
 
 function authApiRequest(method, params="", accessToken="", onSuccess=undefined, onError=undefined){
     /// @description Makes request to API method.
@@ -104,13 +148,15 @@ function authApiGetErrorMessageFromCode(code){
         case 31: return "auth-api-error-password-invalid" // AUTH_PASSWORD_INVALID
         case 32: return "auth-api-error-username-invalid" // AUTH_USERNAME_INVALID
         case 40: return "auth-api-error-invalid-request" // API_INVALID_REQUEST
+        case 41: return "auth-api-error-not-implemented" // API_NOT_IMPLEMENTED
         case 50: return "auth-api-error-cft-invalid-token" // CFT_INVALID_TOKEN
         case 51: return "auth-api-error-cft-email-not-found" // CFT_EMAIL_NOT_FOUND
         case 52: return "auth-api-error-cft-email-already-confirmed" // CFT_EMAIL_ALREADY_CONFIRMED
+        case 60: return "auth-api-error-oauth-client-not-found" // OAUTH_CLIENT_NOT_FOUND
+        case 61: return "auth-api-error-oauth-client-forbidden" // OAUTH_CLIENT_FORBIDDEN
         default: return "auth-api-error-unknown"; // Unknown error code.
     }
 }
-
 
 /*
     Private methods, that should not be used by end-user.
@@ -160,6 +206,8 @@ export {
 
     authApiErrorCode,
 
+    authApiRedirectOAuthAuthorization,
+    authApiGetOAuthAuthorizationUrl,
 
     authApiRequest,
 
@@ -168,6 +216,14 @@ export {
     authMethodSignin,
     authMethodSignup,
     authMethodVerify,
+    authMethodChangelog,
     authMethodEmailConfirm,
-    authMethodEmailResendConfirmation
+    authMethodEmailResendConfirmation,
+
+    authMethodOAuthDirect,
+    authMethodOAuthAuthorize,
+    authMethodOAuthToken,
+    authMethodOAuthClientGet,
+    authMethodOAuthClientNew,
+    authMethodOAuthClientExpire,
 };
